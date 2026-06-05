fec8ef9278197d2ef85192550eb670a53c90d1fd
francescomoglia.moglia <francesco.moglia@aptus.ai>
2026-06-05 09:25:20 +0200

    Add .nvmrc file, update HTML structure, refine package dependencies, and introduce Typography component styles

---
+++ src/components/atoms/Typography/Typography.tsx
@@ -0,0 +1,36 @@
+import clsx from 'clsx'
+import type { ElementType, HTMLAttributes, ReactNode, RefObject } from 'react'
+import style from './Typography.module.css'
+
+interface TypographyProps<T extends ElementType = 'span'> extends Omit<
+  HTMLAttributes<HTMLElement>,
+  'ref'
+> {
+  children?: ReactNode
+  weight?: 'regular' | 'medium' | 'bold'
+  size?: '2xs' | 'xs' | 's' | 'm' | 'l' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl'
+  ref?: RefObject<HTMLElement | null>
+  as?: T
+  align?: 'left' | 'center' | 'right'
+}
+
+export const Typography = <T extends ElementType = 'span'>({
+  children,
+  weight = 'regular',
+  size = 's',
+  ref,
+  as,
+  align = 'left',
+  ...rest
+}: TypographyProps<T>) => {
+  const Component = as || 'span'
+
+  return (
+    <Component
+      {...rest}
+      ref={ref}
+      className={clsx(style.root, style[weight], style[size], style[align], rest.className)}>
+      {children}
+    </Component>
+  )
+}
