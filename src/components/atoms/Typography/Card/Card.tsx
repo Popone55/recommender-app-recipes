dc98ac04154c292f034754d9647983b5666f0d58
francescomoglia.moglia <francesco.moglia@aptus.ai>
2026-06-05 09:51:20 +0200

    Add Card component with styles and update global styles and variables for improved theming

---
+++ src/components/atoms/Typography/Card/Card.tsx
@@ -0,0 +1,27 @@
+import clsx from 'clsx'
+import type { FC, ReactNode } from 'react'
+import style from './Card.module.css'
+
+const a11yProps = (clickable: boolean) => {
+  if (!clickable) return undefined
+
+  return {
+    role: 'button',
+    tabIndex: 0
+  }
+}
+
+export const Card: FC<{
+  children: ReactNode
+  className?: string
+  onClick?: () => void
+}> = ({ children, className, onClick }) => {
+  return (
+    <div
+      {...a11yProps(!!onClick)}
+      onClick={onClick}
+      className={clsx(style.root, className)}>
+      {children}
+    </div>
+  )
+}
