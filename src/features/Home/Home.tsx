ab576aca31b2ef75e1f155e84cffca0b9fde655c
Popone55 <f.moglia92@gmail.com>
2026-06-07 09:05:11 +0200

    Refactor Home component to use Link for navigation; add cardLink class in CSS for consistent styling of links. This enhances user experience by allowing direct navigation to the recipe search page.

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -2,7 +2,7 @@ import { Button } from '@components/atoms/Button/Button'
 import { Card } from '@components/atoms/Typography/Card/Card'
 import { Typography } from '@components/atoms/Typography/Typography'
 import { SearchByTerm } from '@features/SearchByTerm/SearchByTerm'
-import { useNavigate } from '@tanstack/react-router'
+import { Link, useNavigate } from '@tanstack/react-router'
 import { History } from '../History/History'
 import style from './Home.module.css'
 
@@ -12,24 +12,26 @@ export const Home = () => {
   return (
     <div className={style.root}>
       <div className={style.cards}>
-        <Card
-          className={style.card}
-          onClick={() => navigate({ to: '/recipes/search/basic-preferences' })}>
-          <Typography
-            size="3xl"
-            align="center"
-            weight="bold">
-            Let's find a recipe!
-          </Typography>
-          <Typography align="center">
-            Click the button below to get started. We'll help you find the right recipe for you.
-          </Typography>
-          <Button
-            variant="purple"
-            size="large">
-            Get started
-          </Button>
-        </Card>
+        <Link to="/recipes/search/basic-preferences" className={style.cardLink}>
+          <Card
+            className={style.card}
+            onClick={() => navigate({ to: '/recipes/search/basic-preferences' })}>
+            <Typography
+              size="3xl"
+              align="center"
+              weight="bold">
+              Let's find a recipe!
+            </Typography>
+            <Typography align="center">
+              Click the button below to get started. We'll help you find the right recipe for you.
+            </Typography>
+            <Button
+              variant="purple"
+              size="large">
+              Get started
+            </Button>
+          </Card>
+        </Link>
         <SearchByTerm className={style.searchByTerm} />
         <History className={style.history} />
       </div>
