3cb0d6b3f46a27ea0c4f398c0256184002fd46d9
Popone55 <f.moglia92@gmail.com>
2026-06-07 08:58:14 +0200

    Enhance RecipesSearchLayout with back navigation button and improved styling. Update CSS for better layout alignment and spacing.

--- src/features/RecipesSearch/RecipesSearchLayout/RecipesSearchLayout.tsx
+++ src/features/RecipesSearch/RecipesSearchLayout/RecipesSearchLayout.tsx
@@ -1,10 +1,21 @@
+import { Button } from '@components/atoms/Button/Button'
 import { Card } from '@components/atoms/Typography/Card/Card'
+import { useNavigate } from '@tanstack/react-router'
+import { ArrowLeft } from 'lucide-react'
 import type { FC, ReactNode } from 'react'
 import style from './RecipesSearchLayout.module.css'
 
 export const RecipesSearchLayout: FC<{ children: ReactNode }> = ({ children }) => {
+  const navigate = useNavigate()
   return (
     <div className={style.root}>
+      <Button
+        variant="neutral"
+        size="small"
+        startIcon={<ArrowLeft size={16} />}
+        onClick={() => navigate({ to: '/' })}>
+        Back to home
+      </Button>
       <Card className={style.card}>{children}</Card>
     </div>
   )
